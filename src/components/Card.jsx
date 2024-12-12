6896ffa153c1ef800890bb544419ab319fd4ea75
nwankpa-D-boy <nwankpakelechisamuel@gmail.com>
2024-12-12 20:44:54 +0000

    Fix: This commit aims to fix the cart quantity not displaying

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,39 @@
 import PropTypes from "prop-types";
+import { cartContext } from "../context";
+import { useContext } from "react";
 
-const Card = ({item}) => {
-
-    return (
-    <div className="container-fluid">   
-        <div className="rounded-sm p-3 shadow-sm h-100">
-            <div className="mb-3">
-                <img className="img-fluid w-100" src={item.img} alt="" />
-            </div>
-            <div>
-                <h2 className="fs-6">{item.name}</h2>
-                <h3 className="fs-6">₦{item.price}</h3>
-               <button className="btn btn-primary rounded" onClick={()=>{  
-                }}>
-                     Add to Cart
-                </button>
-            </div>
-         </div> 
-    </div> 
-  )
+const Card = ({ item }) => {
+  const { dispatch } = useContext(cartContext);
+  return (
+    <div className="container-fluid">
+      <div className="rounded-sm p-3 shadow-sm h-100">
+        <div className="mb-3">
+          <img className="img-fluid w-100" src={item.img} alt="" />
+        </div>
+        <div>
+          <h2 className="fs-6">{item.name}</h2>
+          <h3 className="fs-6">₦{item.price}</h3>
+          <button
+            className="btn btn-primary rounded"
+            onClick={() => {
+              dispatch({ type: "ADD_TO_CART", payload: item });
+            }}
+          >
+            Add to Cartdfd
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 Card.propTypes = {
-    item: PropTypes.shape({
-        img: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
-    })
-    
-}
+  item: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }),
+};
 
-export default Card
+export default Card;
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,51 +1,53 @@
+import { useReducer, createContext } from "react";
 
- import {useReducer, createContext} from 'react';
+//initial state
+const intialState = {
+  cart: {},
+};
 
- //initial state
- const intialState = {
-    cart:{}
- };
+const cartContext = createContext({});
 
- const cartContext = createContext({});
-
- function cartReducer(state, action){
-    switch (action.type){
-        case "ADD_TO_CART":
-            
-            const item = state.cart[action.payload.id];
-            return{
-                ...state,
-                cart: {
-                    ...state.cart,
-                    [action.payload.id]: item ? {
-                        ...item,
-                        qty: item.qty +1,
-                    } : {
-                        ...action.payload,
-                        qty:1,
-                    }
-                }
-            }
-        case "REMOVE_FROM_CART":
-            let newCart = {...state.cart};
-            delete newCart[action.payload.id];
-            return {
-                ...state,
-                cart: newCart,
-            }
-        default:
-            return state;
+function cartReducer(state, action) {
+  switch (action.type) {
+    case "ADD_TO_CART": {
+      const item = state.cart[action.payload.id];
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          [action.payload.id]: item
+            ? {
+                ...item,
+                qty: item.qty + 1,
+              }
+            : {
+                ...action.payload,
+                qty: 1,
+              },
+        },
+      };
     }
- }
- // context provider
+    case "REMOVE_FROM_CART": {
+      let newCart = { ...state.cart };
+      delete newCart[action.payload.id];
+      return {
+        ...state,
+        cart: newCart,
+      };
+    }
+    default:
+      return state;
+  }
+}
+// context provider
 
- const Provider = ({children}) =>{
-    const [state, dispatch] = useReducer(cartReducer, intialState);
-    return(
-        <cartContext.Provider value={{state, dispatch}}>
-            {children}
-        </cartContext.Provider>
-    );
- }
+const Provider = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, intialState);
+  return (
+    <cartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </cartContext.Provider>
+  );
+};
 
- export {cartContext, Provider};
+export { cartContext, Provider };
